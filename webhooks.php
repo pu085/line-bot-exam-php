c7faa02885f73492fcf1db65ca7eff9a29039ec6
pu085 <pu085@hotmail.com>
2018-10-09 01:43:40 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -17,14 +17,14 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['source']['userId'];
-			$text2 = $event['source']['userName'];
+			$text2 = $event['source']['displayName'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text.'-'.$text2
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
