cbe67dea1032d4fbc07b062cc5edc8736539c864
pu085 <pu085@hotmail.com>
2018-10-25 02:21:37 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,8 +23,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				  "type": "text",
-   "text": "Hello, world"
+				  'type': 'text',
+   'text': 'Hello, world'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
