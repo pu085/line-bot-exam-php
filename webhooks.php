98d9843148b4062880ef8385be0f8b0da4315841
pu085 <pu085@hotmail.com>
2018-10-25 02:24:15 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,8 +23,9 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				  'type': 'text',
-   'text': 'Hello, world'
+				  	"type"=> "image",
+    "originalContentUrl"=> "http://winwinbiz.net/backoffice/templates/images/logo.png",
+    "previewImageUrl"=>"http://winwinbiz.net/backoffice/templates/images/logo.png"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
