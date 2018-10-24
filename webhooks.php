e7663db3d0905501067a2497a027c84a1610535f
pu085 <pu085@hotmail.com>
2018-10-25 02:20:08 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -23,9 +23,8 @@ if (!is_null($events['events'])) {
 
 			// Build message to reply back
 			$messages = [
-				 "type": "video",
-      				"originalContentUrl": "https://example.com/original.mp4",
-                                "previewImageUrl": "https://example.com/preview.jpg"
+				  "type": "text",
+   "text": "Hello, world"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
